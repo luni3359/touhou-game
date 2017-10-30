44178f1191e2031a75d11803ed0dc9a57c86a9d8
luni <lunibackup@outlook.com>
2017-10-29 18:43:28 -0600

    Add canvas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-  'use strict';
+'use strict';
+var canvas, ctx;
 
-  window.onload = function() {
-    
-  };
+function startGame () {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+window.addEventListener('load', function () {
+  canvas = document.getElementById('game');
+  ctx = canvas.getContext('2d');
+
+  startGame();
+});
