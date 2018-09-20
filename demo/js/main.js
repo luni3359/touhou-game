3c18b7a582d7221cf8c682a09f0efc4c85670c5b
luni3395 <lunibackup@outlook.com>
2018-09-19 23:44:53 -0500

    Use game object

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,3 +1,7 @@
+var touhouGame;
+
 window.addEventListener("load", () => {
+    touhouGame = new BulletGame.BulletGame(); // Someone please help me avoid this
+    document.body.appendChild(touhouGame.screen.canvas);
     touhouGame.start();
 });
