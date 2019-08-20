d869b6d6fbb6072b208352675c425b251dbd4c4a
luni3395 <lunibackup@outlook.com>
2019-08-20 18:54:43 -0500

    Add Marisa

--- src/main.js
+++ src/main.js
@@ -14,39 +14,60 @@ var config = {
     scene: {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        pack: { // assets to be preloaded
+            files: [
+                {
+                    type: 'atlas',
+                    key: 'menu',
+                    textureURL: './assets/img/menu.png',
+                    atlasURL: './assets/img/menu.json'
+                }
+            ]
+        }
     }
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    // this.load.setBaseURL('https://labs.phaser.io');
+    // loading screen
+    this.add.image(0, 0, 'menu', 'loading_screen');
 
-    // this.load.image('sky', 'assets/skies/space3.png');
+    //#region Loading progress bar
+    // var progressBar = this.add.graphics();
+    // var progressBox = this.add.graphics();
+    // progressBox.fillStyle(0x222222, 0.5);
+    // progressBox.fillRect(240, 270, 320, 50);
+
+    // this.load.on('progress', function (value) {
+    //     console.log(value);
+    //     progressBar.clear();
+    //     progressBar.fillStyle(0xffffff, 1);
+    //     progressBar.fillRect(250, 280, 300 * value, 30);
+    // });
+
+    // this.load.on('fileprogress', function (file) {
+    //     console.log(file.src);
+    // });
+
+    // this.load.on('complete', function () {
+    //     console.log('complete!');
+    // });
+    //#endregion
+
     this.load.setBaseURL('./assets/img');
-    this.load.atlas('menu', 'menu.png', 'menu.json');
+    this.load.atlas('reimu', 'reimu.png', 'reimu_test.json');
 }
 
 function create() {
-    this.add.image(0, 0, 'menu', 'loading_screen');
-    // this.add.image(400, 300, 'sky');
-
-    // var particles = this.add.particles('red');
-
-    // var emitter = particles.createEmitter({
-    //     speed: 100,
-    //     scale: { start: 1, end: 0 },
-    //     blendMode: 'ADD'
-    // });
-
-    // var logo = this.physics.add.image(400, 100, 'logo');
-
-    // logo.setVelocity(100, 200);
-    // logo.setBounce(1, 1);
-    // logo.setCollideWorldBounds(true);
-
-    // emitter.startFollow(logo);
+    // this.add.image(100, 100, 'reimu', 'idle');
+    let player = this.physics.add.image(100, 100, 'reimu', 'idle');
+    // let player = this.add.sprite(100, 100, 'reimu')
+    // player.anims.add('idle', Phaser.Animation.generateFrameNames('reimux', 0, 1, '', 4), 30, true);
+    // player.anims.play('idle');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 }
 
 function update() {
